2cc29ce23ae226b2ba385ffc93ff01e466d51703
l1ttps <l1ttps443@gmail.com>
2024-04-08 18:10:14 +0700

    feat: auto generate module

--- templates/generate.js
+++ templates/generate.js
@@ -1,14 +1,17 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
-const { CaseConverterEnum, generateTemplateFilesCommandLine } = require('generate-template-files');
+const {
+  CaseConverterEnum,
+  generateTemplateFilesCommandLine,
+} = require("generate-template-files");
 
 generateTemplateFilesCommandLine([
   {
-    option: 'create-nestjs-module',
+    option: "create-nestjs-module",
     defaultCase: CaseConverterEnum.PascalCase,
-    entry: { folderPath: './templates/module/' },
-    stringReplacers: ['__module__', '__ts__'],
+    entry: { folderPath: "./templates/module/" },
+    stringReplacers: ["__module__", "__ts__"],
     output: {
-      path: './src/modules/__module__(kebabCase)',
+      path: "./src/modules/__module__(kebabCase)",
       pathAndFileNameDefaultCase: CaseConverterEnum.KebabCase,
       overwrite: true,
     },
---
+++ templates/module/entities/__module__.entity.ts
@@ -0,0 +1,5 @@
+import { BaseEntity } from "src/common/dto/baseEntity";
+import { Entity } from "typeorm";
+
+@Entity("__module__(kebabCase)")
+export class __module__ extends BaseEntity {}
